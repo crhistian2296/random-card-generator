183178fb5151fafd945794608cd7744dee4557a9
Crhistian Alexander Garcia Urbano <garcia.crhistian@correounivalle.edu.co>
2023-03-20 20:55:34 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const cardValues = [
+  "A",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+const suitValues = ["♠", "♣", "♥", "♦"];
+
+const card = () => cardValues[Math.floor(Math.random() * cardValues.length)];
+const suit = () => suitValues[Math.floor(Math.random() * suitValues.length)];
+
+const cardNumber = document.querySelector(".cardNumber");
+const cardSuit = document.querySelectorAll(".cardSuit")[0];
+const cardSuit2 = document.querySelectorAll(".cardSuit")[1];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  cardNumber.textContent = card();
+  cardSuit.textContent = suit();
+  cardSuit2.textContent = cardSuit.innerText;
 };
